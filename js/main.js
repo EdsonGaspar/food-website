4aa944d2866e6e0c5f5fde582c04265086685fd7
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-08-20 09:39:04 +0100

    Fix: fix not finished

--- js/main.js
+++ js/main.js
@@ -28,3 +28,12 @@ sr.reveal ('.shop-content', {delay:300});
 
 // review customer
 sr.reveal ('.review-content,.contact', {delay:300});
+
+//Code Menu Open
+const menu = document.querySelector('#menu-icon');
+const navList = document.querySelector('.nav-list');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navList.classList.toggle('open');
+}
